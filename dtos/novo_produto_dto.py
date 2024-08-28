5530bdb996e4f64aa5b71ad01e3c98c669dac5ee
Ricardo Maroquio <maroquio@gmail.com>
2024-08-28 17:38:56 -0300

    melhorados os validadores de NovoProdutoDTO

--- dtos/novo_produto_dto.py
+++ dtos/novo_produto_dto.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, field_validator
-from datetime import date, datetime, timedelta
 
 from util.validators import *
 
@@ -12,30 +11,25 @@ class NovoProdutoDTO(BaseModel):
 
     @field_validator("nome")
     def validar_nome(cls, v):
-        msg = is_project_name(v, "Nome")
-        if msg:
-            raise ValueError(msg)
+        msg = is_size_between(v, "Nome", 2, 128)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("preco")
     def validar_preco(cls, v):
         msg = is_in_range(v, "Preço", 0.0, 100000.0)
-        if msg:
-            raise ValueError(msg)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("descricao")
     def validar_descricao(cls, v):
         msg = is_not_empty(v, "Descrição")
-        if not msg:
-            msg = is_min_size(v, "Descrição", 16)
-        if msg:
-            raise ValueError(msg)
+        msg = msg or is_size_between(v, "Descrição", 16, 1024)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("estoque")
     def validar_estoque(cls, v):
         msg = is_in_range(v, "Estoque", 0, 1000)
-        if msg:
-            raise ValueError(msg)
+        if msg: raise ValueError(msg)
         return v
